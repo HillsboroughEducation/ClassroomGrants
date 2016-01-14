161df912c6eafe6aa23d32dd659c1d4d269f4bfe
mstivali <matthewstivali@gmail.com>
2016-01-14 14:37:52 -0800

    Completed updates in new project tabbed flow

--- public_html/app/project/project.controller.js
+++ public_html/app/project/project.controller.js
@@ -6,11 +6,11 @@
 	function Project($scope, $http, $state, $rootScope, $stateParams) {
 
 		$scope.updateMode = false;
-
-		loadProjectData();
-
+		$scope.project = {};
 		$scope.steps = ['one', 'two', 'three'];
 		$scope.step = 0;
+
+		loadProjectData();
 
 		$scope.isFirstStep = function() {
 			return $scope.step === 0;
@@ -47,39 +47,25 @@
 		}
 
 		$scope.handleNext = function() {
-			if($scope.isLastStep()) {
-				//save model 
+			if($scope.isLastStep()) {				
+				$scope.project.userId = $rootScope.currentUser._id;
+				$scope.project.projectStatus = "pending";
+				$scope.project.dateCreated = new Date();
+				$http.post('/projectsApi/projects', $scope.project).success(function(response) {
+					$state.go('budget', {'projectId':response._id});
+				}); 
 			} else {
 				$scope.step += 1;
 			}
 		}
 
-		$scope.submitForm = function(project) {
-
-			if($scope.updateMode){
-				$http.put('/projectsApi/projects/' + project._id + '/project', project).success(function(response) {
-					console.log("Did Update: " + response);
-					$state.go('applicant-applications');
-				});
-			} else {
-				$scope.project.userId = $rootScope.currentUser._id;
-				$scope.project.projectStatus = "pending";
-				$scope.project.dateCreated = new Date();
-				console.log($scope.project);
-				$http.post('/projectsApi/projects', $scope.project).success(function(response) {
-					$state.go('budget', {'projectId':response._id});
-				});
-			}
-			
-		};
-
 		function loadProjectData() {
 			if($stateParams.projectId != null) {
-			$scope.updateMode = true;
-			$http.get('/projectsApi/projects/' + $stateParams.projectId + '/project').success(function(response) {
-				$scope.project = response;
-			});
+				$scope.updateMode = true;
+				$http.get('/projectsApi/projects/' + $stateParams.projectId + '/project').success(function(response) {
+					$scope.project = response;
+				});
+			}
 		} 
-		}
 	}
 })();
