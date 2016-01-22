0a190891a0b961f181755b5ce93fbcc6c120f899
Matt <matthewstivali@gmail.com>
2016-01-21 21:19:35 -0500

    Started review modal

---
+++ public_html/app/applications/reviewer/modals/review-editor/review-editor.controller.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict'
+
+	angular.module('app').controller('ReviewEditor', ReviewEditor);
+
+	function ReviewEditor($scope, $http) {
+
+	}
+
+ })();
--- public_html/app/applications/reviewer/reviewer-applications.controller.js
+++ public_html/app/applications/reviewer/reviewer-applications.controller.js
@@ -3,7 +3,25 @@
 
 	angular.module('app').controller('ReviewerApplications', ReviewerApplications);
 
-	function ReviewerApplications($scope, $http, $rootScope) {
+	function ReviewerApplications($scope, $http, $rootScope, $uibModal, $log) {
+
+		$scope.reviewApplication = function(project) {
+
+			$rootScope.project = project;
+
+			var modalInstance = $uibModal.open({
+		      animation: true,
+		      templateUrl: 'app/applications/reviewer/modals/review-editor/review-editor-modal-template.html',
+		      controller: 'ReviewEditor',
+		      size:'lg'
+		    });
+
+		    modalInstance.result.then(function (data) {
+		    	//returns data here
+		    }, function () {
+		      $log.info('Modal dismissed at: ' + new Date());
+		    });
+		}
 
 		var reviewerId = $rootScope.currentUser._id;
 		console.log(reviewerId);
