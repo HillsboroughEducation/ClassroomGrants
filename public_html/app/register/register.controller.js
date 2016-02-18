5d94db0b4d8961ed8b651fb0dd416fc14eabe71d
mstivali <matthewstivali@gmail.com>
2016-02-18 13:01:19 -0800

    Added spinner

--- public_html/app/register/register.controller.js
+++ public_html/app/register/register.controller.js
@@ -3,32 +3,27 @@
 
 	angular.module('app').controller('Register', Register);
 
-	function Register($scope, $http, $state, $rootScope, UsersService, MailService) {
+	function Register($scope, $http, $state, $rootScope, UsersService, MailService, usSpinnerService) {
 
 		$scope.user = {};
 		$scope.error = false;
 
 		$scope.register = function(user) {
 
+			usSpinnerService.spin('spinner-1');
 			UsersService.registerUserAsync(user, "Applicant", "newUser").then(handleSuccess, handleError);		
 
 			function handleSuccess(response) {
 				var user = response.data;
 				MailService.sendRegistrationConfirmationAsync(user).then(function(response) {
+					usSpinnerService.stop('spinner-1');
 					$state.go('login', {"newUser":true});
 				});
-				/*
-				$rootScope.currentUser = response.data;
-				$rootScope.$broadcast('currentUser');
-				$rootScope.loggedIn = true;
-				$rootScope.appInProgress = true;
-				$rootScope.$broadcast('loginStateChanged');
-				$state.go('project',{});
-				*/
 			};
 
 			function handleError(error) {
 				if(error) {
+					usSpinnerService.stop('spinner-1');
 					$scope.error = true;
 					$scope.errorMessage = "An error occurred";
 				}
