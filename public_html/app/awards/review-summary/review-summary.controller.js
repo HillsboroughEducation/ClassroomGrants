52edd75d3912f8aded22e1407a7c3e303ffd53f1
mstivali <matthewstivali@gmail.com>
2016-02-23 13:18:34 -0800

    Added sweet alerts for award and decline buttons

--- public_html/app/awards/review-summary/review-summary.controller.js
+++ public_html/app/awards/review-summary/review-summary.controller.js
@@ -3,7 +3,7 @@
 
 	angular.module('app').controller('ReviewSummary', ReviewSummary);
 
-	function ReviewSummary($scope, $q, $state, $stateParams, $uibModal, ApplicationsService, ReviewsService) {
+	function ReviewSummary($scope, $q, $state, $stateParams, $uibModal, ApplicationsService, ReviewsService, SweetAlert) {
 	
 		$scope.project = {};
 		$scope.reviews = {};
@@ -11,6 +11,39 @@
 		$scope.ratingAverages = {};	
 
 		loadReviewSummaryData($stateParams.project);
+
+		$scope.award = function() {
+			SweetAlert.swal({
+			   title: "Are you sure you want to award this grant?",
+			   text: "This action will mark the application status as awarded",
+			   type: "warning",
+			   showCancelButton: true,
+			   confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, apply award",
+			   cancelButtonText: "No, cancel",
+			   closeOnConfirm: false,
+			   closeOnCancel: false }, 
+			function(isConfirm){ 
+			   if (isConfirm) {
+			      SweetAlert.swal("Awarded!", "This application has been marked as awarded.", "success");
+			   } else {
+			      SweetAlert.swal("Cancelled", "No action has been taken.", "error");
+			   }
+			});
+		}
+
+		$scope.decline = function() {
+			SweetAlert.swal({
+			   title: "Are you sure you wish to decline this applicant?",
+			   text: "This action will mark the application status as declined",
+			   type: "warning",
+			   showCancelButton: true,
+			   confirmButtonColor: "#DD6B55",
+			   confirmButtonText: "Yes, deline award",
+			   closeOnConfirm: false}, 
+			function(){ 
+			   SweetAlert.swal("The application has been declined.");
+			});
+		}
 
 		$scope.seeReview = function(reviews, reviewSection, title) {
 
