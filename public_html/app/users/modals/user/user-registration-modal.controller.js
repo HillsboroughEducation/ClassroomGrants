88f02ed8bc6943407dc296a721cc86cbeee76541
mstivali <matthewstivali@gmail.com>
2016-03-11 12:02:05 -0800

    Fixed updated function on users view

--- public_html/app/users/modals/user/user-registration-modal.controller.js
+++ public_html/app/users/modals/user/user-registration-modal.controller.js
@@ -12,38 +12,61 @@
 		}
 
 		$scope.inviteUser = function(user) {
-			UsersService.initializeNewUserAccount(user).then(function(response) {
-				console.log(response.data.user);
-				if(response.data.user == null) {
-					Notification.error({message:"Invitation Failed. Someone is already registered with the email " + user.email + '.', positionY:'top', positionX: 'center'});
-					return MailService.inviteUser(user, false);
-				} else {
-					return MailService.inviteUser(user, true);
-				}
-			}).then(function(response){
-				console.log(response);
-				if(response.data.message === '2') {
-					$uibModalInstance.close();
-				} else if(response.data.message === '1') {
-					Notification({title: 'Success', message: 'An invitation has been sent and an account has been opened for ' + user.firstName + ' ' + user.lastName + '.'});
-					$uibModalInstance.close();
-				} else if(response.data.message === '0') {
-				  	Notification.error({message:"Your users account was oppened by the invitation failed to send. Please inform the user.", positionY:'top', positionX: 'center'});
-					$uibModalInstance.close();
-				}
-			});
-		}
-		
-		$scope.submitForm = function (user) {
 			if($scope.updateMode) {
-				UsersService.updateUserAsync(user).success(function(response) {
+				UsersService.updateUserInfoAsync(user).success(function(response) {
 					console.log(response);
 					$uibModalInstance.close('Updated a user');
 				});
 			} else {
-				UsersService.registerUserAsync(user, "Admin", user.role).success(function(response){
+				UsersService.initializeNewUserAccount(user).then(function(response) {
+					console.log(response.data.user);
+					if(response.data.user == null) {
+						Notification.error({message:"Invitation Failed. Someone is already registered with the email " + user.email + '.', positionY:'top', positionX: 'center'});
+						return MailService.inviteUser(user, false);
+					} else {
+						return MailService.inviteUser(user, true);
+					}
+				}).then(function(response){
 					console.log(response);
-					$uibModalInstance.close('Registered a new user');
+					if(response.data.message === '2') {
+						$uibModalInstance.close();
+					} else if(response.data.message === '1') {
+						Notification({title: 'Success', message: 'An invitation has been sent and an account has been opened for ' + user.firstName + ' ' + user.lastName + '.'});
+						$uibModalInstance.close();
+					} else if(response.data.message === '0') {
+					  	Notification.error({message:"Your users account was oppened by the invitation failed to send. Please inform the user.", positionY:'top', positionX: 'center'});
+						$uibModalInstance.close();
+					}
+				});
+			}
+		}
+		
+		$scope.submitForm = function (user) {
+			if($scope.updateMode) {
+				UsersService.updateUserInfoAsync(user).success(function(response) {
+					console.log(response);
+					$uibModalInstance.close('Updated a user');
+				});
+			} else {
+				UsersService.initializeNewUserAccount(user).then(function(response) {
+					console.log(response.data.user);
+					if(response.data.user == null) {
+						Notification.error({message:"Invitation Failed. Someone is already registered with the email " + user.email + '.', positionY:'top', positionX: 'center'});
+						return MailService.inviteUser(user, false);
+					} else {
+						return MailService.inviteUser(user, true);
+					}
+				}).then(function(response){
+					console.log(response);
+					if(response.data.message === '2') {
+						$uibModalInstance.close();
+					} else if(response.data.message === '1') {
+						Notification({title: 'Success', message: 'An invitation has been sent and an account has been opened for ' + user.firstName + ' ' + user.lastName + '.'});
+						$uibModalInstance.close();
+					} else if(response.data.message === '0') {
+					  	Notification.error({message:"Your users account was oppened by the invitation failed to send. Please inform the user.", positionY:'top', positionX: 'center'});
+						$uibModalInstance.close();
+					}
 				});
 			}
 		};
